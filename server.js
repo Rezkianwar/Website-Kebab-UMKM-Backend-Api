d6d230b473df43892d17cb4f67c3b36575af7ea5
Rezkianwar <123230737+Rezkianwar@users.noreply.github.com>
2025-06-23 18:37:00 +0700

    edit server.js

--- server.js
+++ server.js
@@ -118,13 +118,13 @@ app.get("/api/health", (req, res) => {
 });
 
 // Serve static files from React build folder in production
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "public")));
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "public")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "public", "index.html"));
-  });
-}
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.join(__dirname, "public", "index.html"));
+//   });
+// }
 
 // Error handler middleware
 app.use(errorHandler);
