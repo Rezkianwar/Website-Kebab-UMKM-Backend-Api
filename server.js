b01fadde7ed3c560a55f733ef25cffa2eae52e8f
Rezkianwar <123230737+Rezkianwar@users.noreply.github.com>
2025-07-05 17:12:57 +0700

    update server.js

--- server.js
+++ server.js
@@ -3,13 +3,6 @@ const express = require("express");
 const path = require("path");
 const dotenv = require("dotenv");
 const cors = require("cors");
-
-const allowedOrigins = [
-  "http://localhost:3000", // Untuk pengembangan lokal frontend
-  "https://your-backend-api-local-test.com", // Jika ada testing lokal backend
-  "https://kebab-umkm.netlify.app", // <<< Pastikan ini ada di daftar!
-  // Tambahkan domain lain jika diperlukan
-];
 const morgan = require("morgan");
 const helmet = require("helmet");
 const cookieParser = require("cookie-parser");
@@ -48,18 +41,7 @@ if (process.env.NODE_ENV === "development") {
 
 // Security middleware
 app.use(helmet());
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    credentials: true, // Penting jika Anda menggunakan cookie atau header otorisasi
-  })
-);
+app.use(cors());
 // app.use(mongoSanitize());
 // app.use(xss());
 // Middleware sanitasi manual (aman untuk req.body & req.query)
@@ -118,13 +100,13 @@ app.get("/api/health", (req, res) => {
 });
 
 // Serve static files from React build folder in production
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static(path.join(__dirname, "public")));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "public")));
 
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.join(__dirname, "public", "index.html"));
-//   });
-// }
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
+  });
+}
 
 // Error handler middleware
 app.use(errorHandler);
